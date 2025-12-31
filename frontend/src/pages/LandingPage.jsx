8d88f6504ddbb1068b8b882f388ccbfe4b339d86
oofie <tdkmike7406@gmail.com>
2025-12-31 14:15:08 +0700

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,26 +1,12 @@
-import { useEffect, useState } from "react";
-import Intro_Section from "../components/Intro-section";
-import Navigation_Bar from "../components/navigationBar";
-import axios from 'axios'
+import Navbar from "../components/Navbar";
+import AboutUsLanding from "../components/AboutUsLanding";
 
-const LandingPage = () => {
-    const [check, setCheck] = useState([]);
-
-    useEffect(()=>{
-        const fetchTestAPI = async () => {
-            const res = await axios.get("http://localhost:5000/api/test");
-            console.log("This is a response of backend",res.data)
-            setCheck(res.data)
-        }
-        fetchTestAPI();
-    },[])
-    return (
-      <>
-       <Navigation_Bar/>
-       <Intro_Section/>
-      <h1 className="text-center">{check?.message}</h1>
-      </>
-    )
-}
-
+ const LandingPage = () => {
+   return (
+  <>
+    <Intro_Section/>
+    <AboutUsLanding />
+  </>
+   )
+ }
 export default LandingPage;
